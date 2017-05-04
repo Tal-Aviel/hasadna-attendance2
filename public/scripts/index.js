3987c5639b895a9ce128b5366cea715e26b954b3
talpersonal <tal.aviel@mail.huji.ac.il>
2017-05-04 15:31:58 +0300

    Login, saving profile to firebase

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,6 +4,12 @@ let provider = new firebase.auth.GoogleAuthProvider();
 
 let currentUser;
 
+let profile = {};
+
+let fields = ["inputFirstName", "inputLastName", "selectProject", "inputLinkedIn", "inputMAC"];
+
+let loadingDiv = $("<div>").addClass("loading").text("Loading...").appendTo("body");
+
 
 firebase.auth().onAuthStateChanged(user => {
     if (!user) {
@@ -15,13 +21,40 @@ firebase.auth().onAuthStateChanged(user => {
 });
 
 function start() {
-    console.log(currentUser);
     $("#spanUserName").text(currentUser.displayName);
+    loadProfile();
+}
+
+function loadProfile() {
+    let uid = currentUser.uid;
+    firebase.database().ref("profiles").child(uid).once('value').then(snapshot => {
+        loadingDiv.remove();
+        if (!snapshot) return;
+
+        profile = snapshot.val() || {};
+
+        fields.forEach(field => {
+            $(`#${field}`).val(profile[field]);
+        });
+    });
 }
 
 $("#btnSubmit").click(() => {
     let uid = currentUser.uid;
 
-   // Save profile in Firebase
-   // firebase.database().ref("profiles").child(uid)
+    let updates = {};
+    fields.forEach(field => {
+        let val = $(`#${field}`).val();
+        if (val) {
+            updates[field] = val;
+        }
+    });
+
+    firebase.database().ref("profiles").child(uid).update(updates).then(() => {
+        //<div class="alert alert-success" role="alert">
+        let successDiv = $("<div>").addClass("alert alert-success").text("הפרופיל עודכן בהצלחה").hide();
+        $("body").append(successDiv);
+        successDiv.show(400);
+        setTimeout(() => successDiv.hide(400, () => successDiv.remove()), 2000);
+    }).catch(e => console.error(e));
 });
